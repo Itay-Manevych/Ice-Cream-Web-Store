66d40d241b419b8059b71b62cbc6fbe6f2d855a4
Br3adMan1 <118209241+Br3adMan1@users.noreply.github.com>
2023-07-25 14:39:32 +0300

    fixed package.json

--- services/ModelService.js
+++ services/ModelService.js
@@ -1,10 +1,10 @@
 
-const createInstance = async (Model,data) => {
+const createInstance = async (Model, data) => {
     const model_instance= new Model(data);
     return await model_instance.save();
 }
 
-const getInstanceById = async (Model,id) => {
+const getInstanceById = async (Model, id) => {
     return await Model.getModelById(id);
 }
 
@@ -12,11 +12,11 @@ const getAllInstances = async (Model) => {
     return await Model.find({});    
 }
 
-const updateInstance = async (Model,id,updated_data) => {
+const updateInstance = async (Model, id, updated_data) => {
     return await Model.findByIdAndUpdate(id,updated_data,{new: true});
 }
 
-const deleteInstance = async (Model,id) => {
+const deleteInstance = async (Model, id) => {
     return await Model.findByIdAndDelete(id);
 }
 
