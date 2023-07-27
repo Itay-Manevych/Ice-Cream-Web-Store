1549eb71548a9fc1be153cf524ba6dfc81c97f83
Br3adMan1 <118209241+Br3adMan1@users.noreply.github.com>
2023-07-27 13:46:11 +0300

    changed from genric controller to specific ones

---
+++ Controllers/product.js
@@ -0,0 +1,70 @@
+import { ProductService } from "../Services/product";
+
+const createProduct = async (req, res) => {
+    try {
+        const new_product = await ProductService.createProduct(req.body);
+        res.status(201).json(new_product);
+    }
+    catch(error) {
+        res.status(500).json({
+        error: 'Error creating an product', 
+        message: error.message
+        });
+    }
+}
+
+const getProductById = async (req, res) => {
+    try {
+        const product = await ProductService.getProductById(req.body.id);
+        res.status(201).json(product);
+    }
+    catch(error) {
+        res.status(500).json({
+            error: 'Error finding an product by id',
+            message: error.message
+        });
+    }
+}
+
+const getAllProducts = async (req, res) => {
+    try {
+        const Products = await ProductService.getAllProducts();
+        res.status(201).json(products);
+    }
+    catch(error) {
+        res.status(500).json({
+            error: 'Error getting all products',
+            message: error.message
+        })
+    }
+}
+
+const updateProduct = async (req, res) => {
+    try {
+        const updated_product = await ProductService.updateProduct(req.body.id, req.body);
+        res.status(201).json(updated_product);
+    }
+
+    catch(error) {
+        res.status(500).json({
+            error: 'Error updating an product',
+            message: error.message
+        })
+    }
+}
+
+const deleteProduct = async (req, res) => {
+    try {
+        const deleted_product = await ProductService.deleteProduct(req.body.id);
+        res.status(201).json(deleted_product);
+    }
+
+    catch(error) {
+        res.status(500).json({
+            error: 'Error deleting an product',
+            message: error.message
+        })
+    }
+}
+
+export const ProductController = {createProduct, getProductById, getAllProducts, updateProduct, deleteProduct};
