10bc85634202f3811006e4c64c0911281e956bdb
ItayFrisk <itay.manevych@gmail.com>
2023-07-29 13:45:39 +0300

    fixed schemas

--- Models/Schemas/order.js
+++ Models/Schemas/order.js
@@ -9,7 +9,15 @@ const Order = new mongoose.Schema({
     products_info: Cart.schema,
     phone_number: Number,
     address: Address.schema,
-    amount: Number,
+    amount: {
+        type: Number,
+        validate: {
+            validator: (value) => {
+                return value > 0;
+            },
+            message: `${value} is not a valid value`,
+        },
+    },
     date: Date,
     status: { type: Number, enum: [0,1,2,3] } // pending = 0, approved = 1, on-the-way = 2, delivered = 3  
 }, {
