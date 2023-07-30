b9eba5d8a8c7fc966d395f8e76f9320a49972f02
ItayFrisk <itay.manevych@gmail.com>
2023-07-31 00:04:46 +0300

    updated the category and product services

--- Services/category.js
+++ Services/category.js
@@ -5,20 +5,23 @@ const createCategory = async (data) => {
     return await new_category.save();
 }
 
-const getCategoryById = async (id) => {
-    return await Category.findById(id);
+const getCategoryByName = async (name) => {
+    const filter = { name: name };
+    return await Category.findOne(filter);
 }
 
 const getAllCategories = async () => {
     return await Category.find({});    
 }
 
-const updateCategory = async (id, updated_data) => {
-    return await Category.findByIdAndUpdate(id, updated_data, {new: true});
+const updateCategory = async (name, updated_data) => {
+    const filter = { name: name }
+    return await Category.findOneAndUpdate(filter, updated_data, {new: true});
 }
 
-const deleteCategory = async (id) => {
-    return await Category.findByIdAndDelete(id);
+const deleteCategory = async (name) => {
+    const filter = { name: name }
+    return await Category.findOneAndDelete(filter);
 }
 
-export const CategoryService = {createCategory, getCategoryById, getAllCategories, updateCategory, deleteCategory};
+export const CategoryService = {createCategory, getCategoryByName, getAllCategories, updateCategory, deleteCategory};
