905f957e7642a838fcbb025b1fb8caeb464ea46d
ItayFrisk <itay.manevych@gmail.com>
2023-08-19 15:38:24 +0300

    (Feat) updated admin dashboard

--- Routes/dashboard.js
+++ Routes/dashboard.js
@@ -1,11 +1,13 @@
 import express from "express"
 import { UserController } from "../Controllers/user.js";
+import { CategoryController } from "../Controllers/category.js";
 const DashboardRouter = express.Router();
 
 DashboardRouter.route('/')
     .get(async (req, res) => {
         const user = await UserController.getUserByToken(req, res);
-        res.render('./Dashboard/dashboard',{user: user});
+        const categories = await CategoryController.getAllCategories(req,res);
+        res.render('./Dashboard/dashboard',{user: user, categories: categories});
     })
     .post(async (req,res) => {
         const user = await UserController.destroyCookie(req, res);
