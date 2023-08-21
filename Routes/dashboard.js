7155fd554cb1253a03de5dec26284bc8d93f750c
ItayFrisk <itay.manevych@gmail.com>
2023-08-21 23:33:43 +0300

    (Feat) created update product feature

--- Routes/dashboard.js
+++ Routes/dashboard.js
@@ -1,13 +1,15 @@
 import express from "express"
 import { UserController } from "../Controllers/user.js";
+import { ProductController } from "../Controllers/product.js";
 import { CategoryController } from "../Controllers/category.js";
 const DashboardRouter = express.Router();
 
 DashboardRouter.route('/')
     .get(async (req, res) => {
         const user = await UserController.getUserByToken(req, res);
+        const products = await ProductController.getAllProducts(req, res);
         const categories = await CategoryController.getAllCategories(req, res);
-        res.render('./Dashboard/dashboard',{user: user, categories: categories});
+        res.render('./Dashboard/dashboard',{user: user, products: products, categories: categories});
     })
     .post(async (req,res) => {
         const user = await UserController.destroyCookie(req, res);
