faa089484261947718bccbab557f9191c836d5f0
ItayFrisk <itay.manevych@gmail.com>
2023-08-30 23:24:14 +0300

    (Patch), resolved bug in sales heatmap

--- Views/Dashboard/Admin/Admin-Statistics-Display/Graphs/salesHeatmap.js
+++ Views/Dashboard/Admin/Admin-Statistics-Display/Graphs/salesHeatmap.js
@@ -22,10 +22,10 @@ else {
     const margin = { top: 0, right: 25, bottom: 30, left: 150 },
     width = 800 - margin.left - margin.right,
     height = 450 - margin.top - margin.bottom;
-
     const svg = d3
         .select("#sales-heatmap")
         .append("svg")
+        .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
         .attr("id", "sales-heatmap-svg")
         .append("g")
@@ -37,19 +37,19 @@ else {
         const date = order.date; 
         const products = order.products_info;
         products.forEach(product => {
-            const product_name = product.product_name;
-        
-            const time_value = date.slice(0, 10);
-        
-            if (!aggregated_sales_data[time_value]) {
-                aggregated_sales_data[time_value] = {};
+            const productName = product.product_name;
+
+            const timeValue = date.slice(0, 10);
+
+            if (!aggregated_sales_data[timeValue]) {
+                aggregated_sales_data[timeValue] = {};
             }
-        
-            if (!aggregated_sales_data[time_value][product_name]) {
-                aggregated_sales_data[time_value][product_name] = 0;
+
+            if (!aggregated_sales_data[timeValue][productName]) {
+                aggregated_sales_data[timeValue][productName] = 0;
             }
-        
-            aggregated_sales_data[time_value][product_name]++;
+
+            aggregated_sales_data[timeValue][productName]++;
         });
     });
 
@@ -64,7 +64,10 @@ else {
         });
     });
 
-    const time_groups = Array.from(new Set(heatmap_data.map(d => d.time)))
+    const data = heatmap_data;
+
+    const time_groups = Array.from(new Set(data.map(d => d.time)))
+    const variables = unique_products;
 
     const x = d3.scaleBand()
         .range([0, width])
@@ -73,7 +76,7 @@ else {
 
     const y = d3.scaleBand()
         .range([height, 0])
-        .domain(unique_products)
+        .domain(variables)
         .padding(0.25)
         .round(true) 
 
@@ -82,16 +85,15 @@ else {
         .attr("transform", `translate(0, ${height})`)
         .call(d3.axisBottom(x).tickSize(0))
         .select(".domain").remove()
-
     svg.append("g")
         .style("font-size", 15)
         .call(d3.axisLeft(y).tickSize(0))
         .select(".domain").remove();
 
     const min_value = 1;
-    const max_value = d3.max(heatmap_data, d => d.value);
+    const max_value = d3.max(data, d => d.value);
 
-    const generateColor = d3.scaleLog()
+    const myColor = d3.scaleLog()
         .domain([min_value, max_value])
         .range(["#b3e2cd","#fdcdac","#cbd5e8","#f4cae4","#e6f5c9","#fff2ae","#f1e2cc","#cccccc"]);
 
@@ -101,46 +103,48 @@ else {
       .attr("class", "tooltip")
       .style("background-color", "white")
       .style("border", "solid")
-      .style("border-width", "0.125rem")
-      .style("border-radius", "0.313rem")
-      .style("padding", "0.313rem");
+      .style("border-width", "2px")
+      .style("border-radius", "5px")
+      .style("padding", "5px");
 
-    const mouseover = function() {
-        tooltip
-          .style("opacity", 1)
-        d3.select(this)
-          .style("stroke", "#C0C0C0")
-          .style("opacity", 1)
+    const mouseover = function(event,d) {
+      tooltip
+        .style("opacity", 1)
+      d3.select(this)
+        .style("stroke", "#C0C0C0")
+        .style("opacity", 1)
     }
 
-    const mousemove = function(d) {
+    const mousemove = function(event, d) {
         tooltip
           .html(`Product: ${d.variable}<br>Orders: ${d.value}`)
-          .style("opacity", 1);
+          .style("opacity", 1)
     };
 
-    const mouseleave = function() {
-        tooltip
-          .style("opacity", 0)
-        d3.select(this)
-          .style("stroke", "none")
-          .style("opacity", 0.8)
+    const mouseleave = function(event,d) {
+      tooltip
+        .style("opacity", 0)
+      d3.select(this)
+        .style("stroke", "none")
+        .style("opacity", 0.8)
     }
 
     svg.selectAll()
-        .data(heatmap_data) 
+        .data(data, d => d.time + ':' + d.variable) 
         .join("rect")
         .attr("x", function(d) { return x(d.time); })
         .attr("y", function(d) { return y(d.variable); })
-        .attr("rx", "0.25rem")
-        .attr("ry", "0.25rem")
+        .attr("rx", 4)
+        .attr("ry", 4)
         .attr("width", x.bandwidth())
-        .attr("height", "0.938rem")
+        .attr("height", 15)
         .style("fill", function(d) {
-            const salesValue = d.value;
-            const normalizedSales = (salesValue - min_value) / (max_value - min_value) || 0;
-            return generateColor(normalizedSales);
+            const sales_value = d.value;
+            const normalizedSales = (sales_value - min_value) / (max_value - min_value) || 0;
+            return myColor(normalizedSales);
         })
+        .style("stroke-width", 4)
+        .style("stroke", "none")
         .style("opacity", 0.8)
         .on("mouseover", mouseover)
         .on("mousemove", mousemove)
